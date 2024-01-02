62902f190f3561376e4564ba6b1393fc6e740a8e
vichin95 <148530524+vichin95@users.noreply.github.com>
2024-01-02 16:33:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const OS = require('os');
-const ENV = 'DEV';
+const ENV = 'PROD';
 
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from blue deployment version3 by Vishal';
+  const msg = 'Hello from Green Environmet version by Vishal';
   res.send(getPage(msg));
 });
 

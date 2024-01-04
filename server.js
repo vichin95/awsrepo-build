e031acaffeb487b345401d1fea114f3bb162f62a
vishalsundaramgs <120542027+vishalsundaramgs@users.noreply.github.com>
2024-01-04 21:05:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ function getPage(message) {
     + "    <p>ENVIRONMENT: " + ENV + "</p>\n"
     + "  </div>\n"
     + "  <div class=\"middle\">\n"
-    + "    <h1>Host/container name</h1>\n"
+    + "    <h1>Host/container name 2</h1>\n"
     + "    <hr>\n"
     + "    <p>" + OS.hostname() + "</p>\n"
     + "  </div>\n"

fcdfc8f799e121e9890cced2c85ba6d716390cfc
vishalsundaramgs <120542027+vishalsundaramgs@users.noreply.github.com>
2024-01-05 11:52:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const ENV = 'DEV';
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from Green Environmet version4 by VishalSundaramGS';
+  const msg = 'Hello from Green Environmet version5 by VishalSundaramGS';
   res.send(getPage(msg));
 });
 

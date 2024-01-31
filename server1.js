90dee081982c00819ce2e8ce7b934ab2d8e87021
vishalsundaramgs <120542027+vishalsundaramgs@users.noreply.github.com>
2024-01-31 11:27:28 +0530

    Update server1.js

--- server1.js
+++ server1.js
@@ -12,7 +12,7 @@
 #const app = express();
 #app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from Change 1';
+  const msg = 'Hello from Change 2';
   res.send(getPage(msg));
 });
 

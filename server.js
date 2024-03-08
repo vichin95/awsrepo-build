45ebfba812f205e431a1f2472700ddbed4075c53
vishalsundaramgs <120542027+vishalsundaramgs@users.noreply.github.com>
2024-03-08 23:58:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const OS = require('os');
-const ENV = 'PROD';
+const ENV = 'NEWPROD';
 
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from Green Environmet version by Vishal before changes';
+  const msg = 'Hello from Green Environment version by Vishal after changes';
   res.send(getPage(msg));
 });
 
